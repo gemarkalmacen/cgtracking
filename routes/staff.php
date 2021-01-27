f646c4a8e1ee0863a6751edba5e8dc4d63f602f9
Marwen <marwenvaleroso@gmail.com>
2021-01-27 14:34:38 +0800

    Feature 6: Changes on Routes

--- routes/staff.php
+++ routes/staff.php
@@ -18,45 +18,57 @@ Route::group(['prefix' => 'staff'], function() {
     include('staff_ajax.php');
 });
 
-Route::group(['prefix' => 'staff-panel', 'namespace' => 'Staff'], function () {
-    Route::post('/login', 'AuthController@login')->name('staff.auth.login');
-    Route::get('/', 'LoginController@index')->name('staff.login');
-    Route::group(['middleware' => ['auth:users']], function () {
-        // Logout
-        Route::get('/logout', 'AuthController@logout')->name('staff.auth.logout');
-        // Dashboard
-        Route::group(['namespace' => 'Dashboard'], function () {
-            Route::get('/dashboard', 'DashboardController@index')->name('staff.dashboard');
-        });
+Route::post('/login', 'staff\AuthController@login')->name('staff.auth.login');
+Route::get('/', 'staff\LoginController@index')->name('staff.login');
+Route::group(['middleware' => ['auth:users']], function () {
+    // Logout
+    Route::get('/logout', 'staff\AuthController@logout')->name('staff.auth.logout');
+    // Dashboard
+    Route::group(['namespace' => 'staff\Dashboard'], function () {
+        Route::get('/dashboard', 'DashboardController@index')->name('staff.dashboard');
     });
-
-    // users
-    Route::group(['namespace' => 'Users'], function () {
-        Route::resource('users', "UserController", [
-            'names' => [
-                'index' => "staff.users.index",
-                'create' => "staff.users.create",
-                'store' => "staff.users.store",
-                'show' => "staff.users.show",
-                'edit' => "staff.users.edit",
-                'update' => "staff.users.update",
-                'destroy' => "staff.users.destroy",
-            ]
-        ]);
-    });
-
-    Route::group(['namespace' => 'Roles'], function () {
-        Route::resource('roles', "RoleController", [
-            'names' => [
-                'index' => "staff.roles.index",
-                'create' => "staff.roles.create",
-                'store' => "staff.roles.store",
-                'show' => "staff.roles.show",
-                'edit' => "staff.roles.edit",
-                'update' => "staff.roles.update",
-                'destroy' => "staff.roles.destroy",
-            ]
-        ]);
-    });
-
 });
+
+// users
+Route::group(['namespace' => 'staff\Users'], function () {
+    Route::resource('users', "UserController", [
+        'names' => [
+            'index' => "staff.users.index",
+            'create' => "staff.users.create",
+            'store' => "staff.users.store",
+            'show' => "staff.users.show",
+            'edit' => "staff.users.edit",
+            'update' => "staff.users.update",
+            'destroy' => "staff.users.destroy",
+        ]
+    ]);
+});
+
+Route::group(['namespace' => 'staff\Roles'], function () {
+    Route::resource('roles', "RoleController", [
+        'names' => [
+            'index' => "staff.roles.index",
+            'create' => "staff.roles.create",
+            'store' => "staff.roles.store",
+            'show' => "staff.roles.show",
+            'edit' => "staff.roles.edit",
+            'update' => "staff.roles.update",
+            'destroy' => "staff.roles.destroy",
+        ]
+    ]);
+});
+
+Route::group(['namespace' => 'staff\infimos'], function () {
+    Route::resource('infimos', "InfimosController", [
+        'names' => [
+            'index' => "staff.infimos.index",
+            'create' => "staff.infimos.create",
+            'store' => "staff.infimos.store",
+            'show' => "staff.infimos.show",
+            'edit' => "staff.infimos.edit",
+            'update' => "staff.infimos.update",
+            'destroy' => "staff.infimos.destroy",
+        ]
+    ]);
+});
+
