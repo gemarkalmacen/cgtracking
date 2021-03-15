1e5efea7ecd9d5ebff1d6dc611bf6d3d7e7dfd4a
Gemark M. Almacen <gmrk08@gmail.com>
2021-03-15 15:47:51 +0800

    initial commit

--- app/Services/CsvFileImporter/Granteelists.php
+++ app/Services/CsvFileImporter/Granteelists.php
@@ -31,13 +31,37 @@ class Granteelists
                 
                 /** 
                  * ! archived grantee_lists_data into table archive_grantee_lists
-                 * */ 
-                $query_copy = DB::insert('
-                INSERT INTO 
-                    archive_grantee_lists(id, region, province, municipality, barangay, purok, `address`, hh_id, entryid, lastname, firstname, middlename, extensionname, birthday, age, clientstatus, member_status, registrationstatus, sex, relationship_to_hh_head, ipaffiliation, hh_set, `group`, mothers_maiden, date_of_enumeration, lbp_account_number, mode_of_payment, date_tagged_hhstatus, tagged_by, date_registered, created_at, updated_at, upload_history_id)
-                SELECT id, region, province, municipality, barangay, purok, `address`, hh_id, entryid, lastname, firstname, middlename, extensionname, birthday, age, clientstatus, member_status, registrationstatus, sex, relationship_to_hh_head, ipaffiliation, hh_set, `group`, mothers_maiden, date_of_enumeration, lbp_account_number, mode_of_payment, date_tagged_hhstatus, tagged_by, date_registered, created_at, updated_at, upload_history_id FROM grantee_lists');
+                 * */
+                // $query_copy = DB::insert('
+                // INSERT INTO 
+                //     archive_grantee_lists(id, region, province, municipality, barangay, purok, `address`, hh_id, entryid, lastname, firstname, middlename, extensionname, birthday, age, clientstatus, member_status, registrationstatus, sex, relationship_to_hh_head, ipaffiliation, hh_set, `group`, mothers_maiden, date_of_enumeration, lbp_account_number, mode_of_payment, date_tagged_hhstatus, tagged_by, date_registered, created_at, updated_at, upload_history_id)
+                // SELECT id, region, province, municipality, barangay, purok, `address`, hh_id, entryid, lastname, firstname, middlename, extensionname, birthday, age, clientstatus, member_status, registrationstatus, sex, relationship_to_hh_head, ipaffiliation, hh_set, `group`, mothers_maiden, date_of_enumeration, lbp_account_number, mode_of_payment, date_tagged_hhstatus, tagged_by, date_registered, created_at, updated_at, upload_history_id FROM grantee_lists');
+                $query_copy = 1;
+
+                // echo "run";
+                // $fixed_invalid_date_registered = DB::statement('UPDATE grantee_lists SET date_registered=:date_reg WHERE "0000-00-00" =:dateformatt', ['date_reg' => NULL, 'dateformatt' => "DATE_FORMAT(date_registered,'%Y-%m-%d')"]);
+                // $fixed_invalid_date_registered = DB::raw("UPDATE grantee_lists SET date_registered=NULL WHERE DATE_FORMAT(date_registered,'%Y-%m-%d') = '0000-00-00'");
+                // dd($fixed_invalid_date_registered);
+                // $_grantee_lists = Granteelist::where('active', 1)
+                //         ->where("DATE_FORMAT(date_registered,'%Y-%m-%d') = '0000-00-00'")
+                //         ->update(['date_registered' => NULL]);
+
+                // DB::table('grantee_lists')
+                //     ->where("date_registered", '0000-00-00')
+                //     ->update(['date_registered' => NULL]);
+
+                $affected = DB::update(
+                    'update grantee_lists set date_registered = NULL WHERE date_registered = "0000-00-00"'
+                );
+
+                echo "done";
+                echo "result == ".$affected;
+                dd($affected);
+                // die();
+
                 if($query_copy==TRUE){
-                    $query_delete = DB::table('grantee_lists')->delete();
+                    // $query_delete = DB::table('grantee_lists')->delete();
+                    $query_delete = 1;
                     if($query_delete==TRUE){
                         $query = sprintf('
                             LOAD DATA LOCAL INFILE "%s" 
@@ -56,6 +80,14 @@ class Granteelists
                         ', ($file_path));
 
                         DB::commit();
+
+                        if($query){
+                            $fixed_invalid_date_registered = DB::statement("UPDATE grantee_lists SET date_registered=NULL WHERE '0000-00-00' = DATE_FORMAT(date_registered,'%Y-%m-%d')");
+                            $fixed_invalid_date_tagged_hhstatus = DB::statement("UPDATE grantee_lists SET date_tagged_hhstatus=NULL WHERE '0000-00-00' = DATE_FORMAT(date_tagged_hhstatus,'%Y-%m-%d')");
+                            $fixed_invalid_date_of_enumeration = DB::statement("UPDATE grantee_lists SET date_of_enumeration=NULL WHERE '0000-00-00' = DATE_FORMAT(date_of_enumeration,'%Y-%m-%d')");
+                            $fixed_invalid_birthday = DB::statement("UPDATE grantee_lists SET birthday=NULL WHERE '0000-00-00' = DATE_FORMAT(birthday,'%Y-%m-%d')");
+                        }
+
                         return $query;
                     } else {
                         throw new \ErrorException('Importing Process Failed!');
