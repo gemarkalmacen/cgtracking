3759dabee655b2b06abc698b3819f2ea69a8e010
Gemark M. Almacen <gmrk08@gmail.com>
2022-03-24 16:17:32 +0800

    Update migration table non_emv

---
+++ database/migrations/2022_03_24_151123_update_migration_table_non_emv.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateMigrationTableNonEmv extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('non_emv', function (Blueprint $table) {
+            $table->date('balance_as_of', 150)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('non_emv', function (Blueprint $table) {
+            $table->double('balance_as_of',15,2)->nullable()->change();
+        });
+    }
+}
