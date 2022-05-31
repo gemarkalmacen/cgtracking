8b94af513a6ef1cfb89c8ce25c5ad00e9241fdd2
Reymark <reymarkvaldehueza@gmail.com>
2022-05-31 17:26:49 +0800

    feature/cgt-129 : create migration lib_clientstatus

---
+++ database/migrations/2022_05_31_160318_create_lib_clientstatus.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibClientstatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lib_clientstatus', function (Blueprint $table) {
+            $table->id();
+            $table->string('clientstatus')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lib_clientstatus');
+    }
+}
