c929601da40c119f62c112bd165a0b26d326c9f7
Gemark M. Almacen <gmrk08@gmail.com>
2022-07-20 17:32:28 +0800

    update 5

--- templates/staff/inquiry/index.blade.php
+++ templates/staff/inquiry/index.blade.php
@@ -371,7 +371,7 @@
                                 </div>
                                 <div class="d-flex align-items-center mb-1">
                                     <div>
-                                        <a href="#" class="font-size-h6 text-dark-75 text-hover-primary font-weight-bolder">Date Claimed/Age : @{{ emv.date_claimed.toUpperCase() }}</a>
+                                        <a href="#" class="font-size-h6 text-dark-75 text-hover-primary font-weight-bolder">Date Claimed/Age : @{{ emv.date_claimed }}</a>
                                     </div>
                                 </div>
                                 <div class="d-flex align-items-center mb-1">
