a7bb92cf8f503fe60d0948d607355da58a8eab4f
Gemark M. Almacen <gmrk08@gmail.com>
2022-09-26 14:43:00 +0800

    change Fullname into three text fields (tables affected : emv_database_monitoring & emv_database_monitoring_details)

---
+++ database/migrations/2022_09_26_132807_change_fullname_into_three_text_fields_table_emv_databasemonitoring.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeFullnameIntoThreeTextFieldsTableEmvDatabasemonitoring extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('emv_database_monitoring', function (Blueprint $table) {
+            $table->string('first_name')->nullable()->after('id');
+            $table->string('last_name')->nullable()->after('first_name');
+            $table->string('middle_name')->nullable()->after('last_name');
+            $table->string('ext_name')->nullable()->after('middle_name');
+            $table->dropColumn('full_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('emv_database_monitoring', function (Blueprint $table) {
+            $table->string('full_name');
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('middle_name');
+            $table->dropColumn('ext_name');
+        });
+    }
+}
