f7b920c877384ad14a5b5ca2096940812f67864d
Gemark M. Almacen <gmrk08@gmail.com>
2022-09-30 17:55:31 +0800

    create migration for card_details

--- database/migrations/2022_09_30_163059_create_migration_for_card_details.php
+++ database/migrations/2022_09_30_163059_create_migration_for_card_details.php
@@ -15,7 +15,21 @@ class CreateMigrationForCardDetails extends Migration
     {
         Schema::create('card_details', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('card_number_prefilled', 30)->nullable()->comment('pre filled data from emv_monitoring');
+            $table->string('card_number_system_generated', 30)->nullable()->comment('data from mobile app');
+            $table->string('card_number_inputted', 30)->nullable()->comment('data inputted by validator');
+            $table->string('card_number_series', 30)->nullable();
+            $table->text('distribution_status')->nullable();
+            $table->date('release_date')->nullable();
+            $table->text('release_by')->nullable();
+            $table->text('release_place')->nullable();
+            $table->text('card_physically_presented')->nullable();
+            $table->text('card_pin_is_attached')->nullable();
+            $table->text('reason_not_presented')->nullable();
+            $table->text('reason_unclaimed')->nullable();
+            $table->text('card_replacement_request')->nullable();
+            $table->text('card_replacement_submitted_details')->nullable();
+            $table->text('emv_monitoring_id')->nullable();
             $table->timestamps();
         });
     }
