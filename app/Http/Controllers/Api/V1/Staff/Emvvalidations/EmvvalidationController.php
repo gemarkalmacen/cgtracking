284723d6a216535a129215653581a40e62a0ce39
Gemark M. Almacen <gmrk08@gmail.com>
2022-10-05 14:58:15 +0800

    update api endpoints - pulldata include other_card_holder_name

--- app/Http/Controllers/Api/V1/Staff/Emvvalidations/EmvvalidationController.php
+++ app/Http/Controllers/Api/V1/Staff/Emvvalidations/EmvvalidationController.php
@@ -24,41 +24,41 @@ class EmvvalidationController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id, GetEmvDatabaseMonitoringByHhId $getEmvDatabaseMonitoringByHhId)
-    {
-        $response = $getEmvDatabaseMonitoringByHhId->execute($id);
-        if(!$response){
-            return response()->json([                
-                'status' => __('messages.error'),
-                'description' => __('messages.not_found'),
-            ],404);
-        }
+    // public function show($id, GetEmvDatabaseMonitoringByHhId $getEmvDatabaseMonitoringByHhId)
+    // {
+        // $response = $getEmvDatabaseMonitoringByHhId->execute($id);
+        // if(!$response){
+        //     return response()->json([                
+        //         'status' => __('messages.error'),
+        //         'description' => __('messages.not_found'),
+        //     ],404);
+        // }
         
-        return response()->json([                
-            'status' => __('messages.success'),
-            'description' => __('messages.ok'),
-            'data' =>   [
-                'id' => $response->id,
-                'full_name' => $response->full_name,           
-                'hh_id' => $response->hh_id,
-                'client_status' => $response->client_status,
-                'address' => $response->address,
-                'sex' => $response->sex,
-                'hh_set_group' => $response->hh_set_group,
-                'current_grantee_card_number' => $response->current_grantee_card_number,
-                'other_card_number_1' => $response->other_card_number_1,
-                'other_card_holder_name_1' => $response->other_card_holder_name_1,
-                'other_card_number_2' => $response->other_card_number_2,
-                'other_card_holder_name_2' => $response->other_card_holder_name_2,
-                'other_card_number_3' => $response->other_card_number_3,
-                'other_card_holder_name_3' => $response->other_card_holder_name_3,
-                'upload_history_id' => $response->upload_history_id,
-                'is_updated' => $response->is_updated,
-                'created_at' => !empty($response->created_at) ? $response->created_at->format('Y-m-d H:i:s') : NULL,
-                'updated_at' => !empty($response->updated_at) ? $response->updated_at->format('Y-m-d H:i:s') : NULL,
-            ]
-        ],200);
-    }
+        // return response()->json([                
+        //     'status' => __('messages.success'),
+        //     'description' => __('messages.ok'),
+        //     'data' =>   [
+        //         'id' => $response->id,
+        //         'full_name' => $response->full_name,           
+        //         'hh_id' => $response->hh_id,
+        //         'client_status' => $response->client_status,
+        //         'address' => $response->address,
+        //         'sex' => $response->sex,
+        //         'hh_set_group' => $response->hh_set_group,
+        //         'current_grantee_card_number' => $response->current_grantee_card_number,
+        //         'other_card_number_1' => $response->other_card_number_1,
+        //         'other_card_holder_name_1' => $response->other_card_holder_name_1,
+        //         'other_card_number_2' => $response->other_card_number_2,
+        //         'other_card_holder_name_2' => $response->other_card_holder_name_2,
+        //         'other_card_number_3' => $response->other_card_number_3,
+        //         'other_card_holder_name_3' => $response->other_card_holder_name_3,
+        //         'upload_history_id' => $response->upload_history_id,
+        //         'is_updated' => $response->is_updated,
+        //         'created_at' => !empty($response->created_at) ? $response->created_at->format('Y-m-d H:i:s') : NULL,
+        //         'updated_at' => !empty($response->updated_at) ? $response->updated_at->format('Y-m-d H:i:s') : NULL,
+        //     ]
+        // ],200);
+    // }
 
     public function pulldata($id, PullDataEmvValidationById $pullDataEmvValidationById, CountEmvValidationById $countEmvValidationById)
     {
@@ -80,20 +80,20 @@ class EmvvalidationController extends Controller
         ],200);
     }
 
-    public function updater(UpdaterEmvDatabaseMonitoring $updaterEmvDatabaseMonitoring){
-        $response = $updaterEmvDatabaseMonitoring->execute();
-        if(!$response){
-            return response()->json([                
-                'status' => __('messages.error'),
-                'description' => __('messages.not_found'),
-            ],404);
-        }
+    // public function updater(UpdaterEmvDatabaseMonitoring $updaterEmvDatabaseMonitoring){
+        // $response = $updaterEmvDatabaseMonitoring->execute();
+        // if(!$response){
+        //     return response()->json([                
+        //         'status' => __('messages.error'),
+        //         'description' => __('messages.not_found'),
+        //     ],404);
+        // }
         
-        return response()->json([                
-            'status' => __('messages.success'),
-            'description' => __('messages.ok'),
-            'data' => EmvvalidationResource::collection($response)
-        ],200);
-    }
+        // return response()->json([                
+        //     'status' => __('messages.success'),
+        //     'description' => __('messages.ok'),
+        //     'data' => EmvvalidationResource::collection($response)
+        // ],200);
+    // }
 
 }
