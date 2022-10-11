109ebe786c32fa4945de589e88461bd7040d1726
Gemark M. Almacen <gmrk08@gmail.com>
2022-10-11 19:28:02 +0800

    Add overall_remarks field in emv_validation_details

---
+++ database/migrations/2022_10_11_181916_add_overall_remarks_field_in_emv_validation_details.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOverallRemarksFieldInEmvValidationDetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('emv_validation_details', function (Blueprint $table) {
+            $table->string('overall_remarks')->after('relationship_to_grantee');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('emv_validation_details', function (Blueprint $table) {
+            $table->dropColumn('overall_remarks');
+        });
+    }
+}
