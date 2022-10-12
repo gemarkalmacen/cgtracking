220bc4f4376562be735c8b0ffc35519c2b1c7507
Gemark M. Almacen <gmrk08@gmail.com>
2022-10-12 19:40:04 +0800

    additional field image and additional_image in table grantee_validations

---
+++ database/migrations/2022_10_12_182359_additional_field_image_and_additional_image_in_table_grantee_validations.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AdditionalFieldImageAndAdditionalImageInTableGranteeValidations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('grantee_validations', function (Blueprint $table) {
+            $table->string('image')->nullable()->after('hh_id');
+            $table->string('image_additional')->nullable()->after('additional_image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('grantee_validations', function (Blueprint $table) {
+            $table->string('image');
+            $table->string('image_additional');
+        });
+    }
+}
