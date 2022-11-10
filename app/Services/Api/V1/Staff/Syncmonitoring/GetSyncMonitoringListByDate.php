3eaf7b9ed33cf5ae826a0cd9119d8da63e360657
mavaleroso <60253529+mavaleroso@users.noreply.github.com>
2022-11-10 13:41:43 +0800

    Update Sync Monitoring Listing Endpoints Callback

--- app/Services/Api/V1/Staff/Syncmonitoring/GetSyncMonitoringListByDate.php
+++ app/Services/Api/V1/Staff/Syncmonitoring/GetSyncMonitoringListByDate.php
@@ -2,19 +2,12 @@
 
 namespace App\Services\Api\V1\Staff\Syncmonitoring;
 
-use App\Exceptions\NotFoundException;
 use App\Models\Syncmonitoring;
-use App\Models\UserDetail;
-use Spatie\QueryBuilder\QueryBuilder;
-use Exception;
 use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Exceptions\HttpResponseException;
 
 class GetSyncMonitoringListByDate
 {
-    public function execute($date)
+    public function execute()
     {
         $result = Syncmonitoring::select(
             'sm.user_id',
@@ -41,8 +34,8 @@ class GetSyncMonitoringListByDate
             'sm.sync_at')
         ->from('sync_monitoring AS sm')
         ->leftJoin('users AS u', 'u.id', '=', 'sm.user_id')
-        ->whereDate('sm.sync_at', $date)
         ->groupBy('sm.user_id')
+        ->orderBy('u.username')
         ->get();
 
         return $result;
