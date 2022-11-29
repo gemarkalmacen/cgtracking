b3caa56e7a3d2cee16201ee1f26257b6ceaa2517
mavaleroso <60253529+mavaleroso@users.noreply.github.com>
2022-11-29 15:12:38 +0800

    Fix Bugs on Syncmonitoring Sync_at

--- app/Services/Api/V1/Staff/Syncmonitoring/CreateSyncmonitoring.php
+++ app/Services/Api/V1/Staff/Syncmonitoring/CreateSyncmonitoring.php
@@ -18,20 +18,21 @@ class CreateSyncmonitoring
     {
         DB::beginTransaction();
         try {
-            
+
             $sync_monitoring = new Syncmonitoring;
             $sync_monitoring->sync_counter = $request->sync_counter;
             $sync_monitoring->update_counter = $request->update_counter;
             $sync_monitoring->pulldata_counter = $request->pulldata_counter;
             $sync_monitoring->psgc_counter = $request->psgc_counter;
             $sync_monitoring->user_id = $request->user_id;
+            $sync_monitoring->sync_at = now();
+            $sync_monitoring->created_at = now();
             $sync_monitoring->updated_at = null;
-            
-            if($sync_monitoring->save()){
+
+            if ($sync_monitoring->save()) {
                 DB::commit();
                 return $request->all();
             }
-
         } catch (Exception $ex) {
             DB::rollBack();
             print_r($ex->getMessage());
